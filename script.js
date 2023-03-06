4ccd4ce1aa7b45041434b3e0a9936647b8199dcf
Sniknam <sianiknam@gmail.com>
2023-03-06 13:28:02 -0800

    complete the page view with html and css

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // to do list container
-const toDoListContainer = document.querySelector(".do-list-body");
+// const toDoListContainer = document.querySelector(".do-list-body");
 
-const listItem = [];
+// const listItem = [];
 
-function printListItem(value) {
-  let btnListElm = document.createElement("button");
-  let spanListElm = document.createElement("span");
-  console.log(value);
+// function printListItem(value) {
+//   let btnListElm = document.createElement("button");
+//   let spanListElm = document.createElement("span");
+//   console.log(value);
 
-  // spanListElm.appendChild(inputValue);
-  btnListElm.appendChild(spanListElm);
-  toDoListContainer.appendChild(btnListElm);
-}
+//   // spanListElm.appendChild(inputValue);
+//   btnListElm.appendChild(spanListElm);
+//   toDoListContainer.appendChild(btnListElm);
+// }
 
-// addBtn();
+// // addBtn();
 
-// add botton
-const inputAddbtn = document.querySelector(".add-btn");
+// // add botton
+// const inputAddbtn = document.querySelector(".add-btn");
 
-function addbtn() {
-  // input variable
-  const inputAdd = document.querySelector(".add-input").value;
+// function addbtn() {
+//   // input variable
+//   const inputAdd = document.querySelector(".add-input").value;
 
-  listItem.unshift(inputAdd);
+//   listItem.unshift(inputAdd);
 
-  printListItem(inputAdd);
-}
-inputAddbtn.addEventListener("click", addbtn);
+//   printListItem(inputAdd);
+// }
+// inputAddbtn.addEventListener("click", addbtn);
