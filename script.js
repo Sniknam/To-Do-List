4be9fe71e6a919c18d8946d3707749977abf2e2f
Sniknam <sianiknam@gmail.com>
2023-03-06 12:25:07 -0800

    creating the html and css, and add js

---
+++ script.js
@@ -0,0 +1,29 @@
+// to do list container
+const toDoListContainer = document.querySelector(".do-list-body");
+
+const listItem = [];
+
+function printListItem(value) {
+  let btnListElm = document.createElement("button");
+  let spanListElm = document.createElement("span");
+  console.log(value);
+
+  // spanListElm.appendChild(inputValue);
+  btnListElm.appendChild(spanListElm);
+  toDoListContainer.appendChild(btnListElm);
+}
+
+// addBtn();
+
+// add botton
+const inputAddbtn = document.querySelector(".add-btn");
+
+function addbtn() {
+  // input variable
+  const inputAdd = document.querySelector(".add-input").value;
+
+  listItem.unshift(inputAdd);
+
+  printListItem(inputAdd);
+}
+inputAddbtn.addEventListener("click", addbtn);
