98761dad3e5e8e9bdb9e9df5d5749410dfeba192
Sniknam <sianiknam@gmail.com>
2023-04-11 13:10:19 -0700

    finished cross over listItem

--- script.js
+++ script.js
@@ -8,58 +8,81 @@ listContainer.classList.add("lastBtn");
 
 // add botton
 const inputAddbtn = document.querySelector(".add-btn");
-
+const inputAdd = document.querySelector(".add-input").value;
+const objInput = { lable: inputAdd, isSelected: false };
 const listItem = [];
 
 function printListItem() {
-  const inputAdd = document.querySelector(".add-input").value;
-  let btnListElm = document.createElement("button");
+  listContainer.innerHTML = "";
 
-  btnListElm.addEventListener("click", function (e) {
-    if (inputAdd) {
-      btnListElm.classList.add("first");
-      console.log(inputAdd);
-    } else {
-      btnListElm;
-    }
-    console.log("parent clicked");
-  });
-
-  let listElmValue = document.createTextNode(inputAdd);
-  console.log(listElmValue);
-  if (inputAdd === "") {
-    alert("please write your task");
-  } else {
+  for (let i = 0; i < listItem.length; i++) {
+    let slectedItemObj = listItem[i];
+    let btnListElm = document.createElement("button");
     let listDelElm = document.createElement("button");
-
-    listDelElm.addEventListener("click", function (e) {
-      console.log("child clicked");
-      btnListElm.addEventListener("click", event.stopPropagation());
-      listContainer.removeChild(btnListElm);
-    });
-
+    let listElmValue = document.createTextNode(slectedItemObj.lable);
     listDelElm.classList.add("fa");
     listDelElm.classList.add("fa-trash-o");
-
     btnListElm.appendChild(listDelElm);
     btnListElm.appendChild(listElmValue);
     btnListElm.classList.add("listBtn");
+    if (slectedItemObj.isSelected === true) {
+      btnListElm.classList.add("first");
+    }
     listContainer.appendChild(btnListElm);
 
-    function clearInput() {
-      if (inputAdd != "") {
-        document.querySelector(".add-input").value = "";
+    function cross() {
+      if (slectedItemObj.isSelected === false) {
+        slectedItemObj.isSelected = true;
+      } else {
+        slectedItemObj.isSelected = false;
       }
+      printListItem();
+
+      console.log(listItem);
     }
-    clearInput();
+    btnListElm.addEventListener("click", cross);
   }
+
+  // btnListElm.addEventListener("click", function (e) {
+  //   if (objInput.isSelected === false) {
+  //     btnListElm.classList.add("first");
+
+  //     objInput.isSelected = true;
+  //     listItem.unshift(objInput);
+  //   } else if (objInput.isSelected === true) {
+  //     btnListElm.classList.add("second");
+  //     objInput.isSelected = false;
+  //     listItem.push(objInput);
+  //   } else {
+  //     btnListElm;
+  //   }
+  //   console.log("parent clicked");
+  // });
+  // console.log(listElmValue);
+  // if (inputAdd === "") {
+  //   alert("please write your task");
+  // } else {
+  //   let listDelElm = document.createElement("button");
+  //   listDelElm.addEventListener("click", function (e) {
+  //     console.log("child clicked");
+  //     btnListElm.addEventListener("click", event.stopPropagation());
+  //     listContainer.removeChild(btnListElm);
+  //   });
+
+  //   function clearInput() {
+  //     if (inputAdd != "") {
+  //       document.querySelector(".add-input").value = "";
+  //     }
+  //   }
+  //   clearInput();
+  // }
 }
 
 function addbtn() {
   const inputAdd = document.querySelector(".add-input").value;
+  const objInput = { lable: inputAdd, isSelected: false };
 
   if (inputAdd) {
-    const objInput = { lable: inputAdd, isSelected: false };
     listItem.unshift(objInput);
     console.log(listItem);
   }
